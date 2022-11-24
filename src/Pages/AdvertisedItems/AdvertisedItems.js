76bc970a7ab86eaae13076f25e5b1ecdd039af2a
Mydulislam <mydulislam693852@gmail.com>
2022-11-24 20:58:38 +0600

    advertised section created

---
+++ src/Pages/AdvertisedItems/AdvertisedItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./AdvertesedItems.css"
+const AdvertisedItems = () => {
+    return (
+        <div>
+            <h1 className='text-center text-4xl font-bold my-7'><span className='befor-css'>Advertised</span> Items</h1>
+        </div>
+    );
+};
+
+export default AdvertisedItems;
