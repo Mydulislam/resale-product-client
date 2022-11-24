47548848d314278f98be912da2ef297cfd227dc7
Mydulislam <mydulislam693852@gmail.com>
2022-11-24 09:44:41 +0600

    Login, Signup page create and include routes

--- src/Pages/Routes/Routes/Router.js
+++ src/Pages/Routes/Routes/Router.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Login from "../../Login/Login";
 import Home from "../../Shared/Home/Home/Home";
+import SignUp from "../../SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +12,15 @@ const router = createBrowserRouter([
         {
             path:'/',
             element:<Home></Home>
-        }
+        },
+        {
+            path:'/login',
+            element:<Login></Login>
+        },
+        {
+            path:'/signup',
+            element:<SignUp></SignUp>
+        },
       ]
     },
   ]);
