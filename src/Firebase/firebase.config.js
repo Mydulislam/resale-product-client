43f4247505bc6e384c67651503ec013fdc5d4909
Mydulislam <mydulislam693852@gmail.com>
2022-11-25 22:50:13 +0600

    using react hook form and design login form

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCijXAsVgdY-rzQawItJqmtGep2g178Dw",
+  authDomain: "assignment-12-2d564.firebaseapp.com",
+  projectId: "assignment-12-2d564",
+  storageBucket: "assignment-12-2d564.appspot.com",
+  messagingSenderId: "697997271316",
+  appId: "1:697997271316:web:1326b1687fac9d50a5e201"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState("");
     return (
-        <div>
-            <h1>This is login page</h1>
+        <div className='mt-12 h-[90vh] flex justify-center'>
+            <div className='lg:w-2/6'>
+                <h1 className='text-4xl text-center mb-3'>Login</h1>
+                <div className='border border-solid p-4 rounded-md'>
+                    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text">Email</span></label>
+                            <input type="email" {...register("email")} placeholder="Your Email" className="input input-bordered w-full" />
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text">Password</span></label>
+                            <input type="password" {...register("password")} placeholder="Password" className="input input-bordered w-full" />
+                        </div>
+                        <input className='btn btn-primary w-full text-white mt-5' value='Login' type="submit" />
+                    </form>
+                    <div className="divider">OR</div>
+                    <button className="btn btn-outline uppercase w-full">Login With Google</button>
+                </div>
+
+            </div>
+
         </div>
     );
 };
